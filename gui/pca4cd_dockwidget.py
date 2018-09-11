2a12f94a8875ee1cced4fd21e30be79cf2b2f0e2
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2018-09-11 17:07:02 -0500

    Adjusted and reordered files

---
+++ gui/pca4cd_dockwidget.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ PCA4CD
+                                 A QGIS plugin
+ Principal components analysis for change detections
+                              -------------------
+        copyright            : (C) 2018 by Xavier Corredor Llano, SMByC
+        email                : xcorredorl@ideam.gov.co
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt5 import QtGui, QtWidgets, uic
+from PyQt5.QtCore import pyqtSignal
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'pca4cd_dockwidget_base.ui'))
+
+
+class PCA4CDDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
+
+    closingPlugin = pyqtSignal()
+
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(PCA4CDDockWidget, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
+
+    def closeEvent(self, event):
+        self.closingPlugin.emit()
+        event.accept()
