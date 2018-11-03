f5c0ff17c17845a5077271cf3a9272cbab785174
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2018-11-03 18:31:38 -0500

    Several improve the new PCA processing using nodata for pair-mask the not valid data across all dimensions before process the PCA (better memory use and time)

--- core/pca_dask_gdal.py
+++ core/pca_dask_gdal.py
@@ -46,40 +46,56 @@ def pca(A, B, n_pc, estimator_matrix, out_dir, n_threads, block_size, nodata=Non
     dask.config.set(pool=ThreadPool(n_threads))
 
     raw_image = []
+    nodata_mask = None
     src_ds_A = gdal.Open(A, gdal.GA_ReadOnly)
+    src_ds_B = None
     for band in range(src_ds_A.RasterCount):
-        raw_image.append(src_ds_A.GetRasterBand(band + 1).ReadAsArray().flatten())
-
+        ds = src_ds_A.GetRasterBand(band + 1).ReadAsArray().flatten().astype(np.float32)
+        if nodata is not None:
+            nodata_mask = ds==nodata if nodata_mask is None else np.logical_or(nodata_mask, ds==nodata)
+        raw_image.append(ds)
     if B is not None:
         src_ds_B = gdal.Open(B, gdal.GA_ReadOnly)
         for band in range(src_ds_B.RasterCount):
-            raw_image.append(src_ds_B.GetRasterBand(band + 1).ReadAsArray().flatten())
+            ds = src_ds_B.GetRasterBand(band + 1).ReadAsArray().flatten().astype(np.float32)
+            if nodata is not None:
+                nodata_mask = np.logical_or(nodata_mask, ds==nodata)
+            raw_image.append(ds)
 
+    # pair-masking data, let only the valid data across all dimensions/bands
+    if nodata is not None:
+        raw_image = [b[~nodata_mask] for b in raw_image]
     # flat each dimension (bands)
-    flat_dims = da.vstack(raw_image).rechunk((1, block_size**2))
-
+    flat_dims = da.vstack(raw_image).rechunk((1, block_size ** 2))
+    # bands
     n_bands = flat_dims.shape[0]
 
     ########
-    # set the center value using the nodata or zero
-    if nodata is not None:
-        center_value = nodata
-    else:
-        center_value = 0
+    # compute the mean of each band, in order to center the matrix.
+    band_mean = []
+    for i in range(n_bands):
+        band_mean.append(dask.delayed(np.mean)(flat_dims[i]))
+    band_mean = dask.compute(*band_mean)
 
     ########
     # compute the matrix correlation/covariance
     estimation_matrix = np.empty((n_bands, n_bands))
-    for i in range(n_bands):
-        deviation_scores_band_i = flat_dims[i] - center_value
-        for j in range(i, n_bands):
-            deviation_scores_band_j = flat_dims[j] - center_value
-            if estimator_matrix == "Correlation":
+    if estimator_matrix == "Correlation":
+        for i in range(n_bands):
+            deviation_scores_band_i = flat_dims[i] - band_mean[i]
+            for j in range(i, n_bands):
+                deviation_scores_band_j = flat_dims[j] - band_mean[j]
                 estimation_matrix[j][i] = estimation_matrix[i][j] = \
                     da.corrcoef(deviation_scores_band_i, deviation_scores_band_j)[0][1]
-            if estimator_matrix == "Covariance":
+    if estimator_matrix == "Covariance":
+        for i in range(n_bands):
+            deviation_scores_band_i = flat_dims[i] - band_mean[i]
+            for j in range(i, n_bands):
+                deviation_scores_band_j = flat_dims[j] - band_mean[j]
                 estimation_matrix[j][i] = estimation_matrix[i][j] = \
                     da.cov(deviation_scores_band_i, deviation_scores_band_j)[0][1]
+    # free mem
+    del raw_image, flat_dims, nodata_mask, src_ds_A, src_ds_B, ds
 
     ########
     # calculate eigenvectors & eigenvalues of the matrix
@@ -99,9 +115,25 @@ def pca(A, B, n_pc, estimator_matrix, out_dir, n_threads, block_size, nodata=Non
     ########
     # save the principal components separated in tif images
 
+    raw_image = []
+    src_ds_A = gdal.Open(A, gdal.GA_ReadOnly)
+    src_ds_B = None
+    for band in range(src_ds_A.RasterCount):
+        ds = src_ds_A.GetRasterBand(band + 1).ReadAsArray().flatten().astype(np.float32)
+        if nodata is not None:
+            ds[ds == nodata] = band_mean[band]
+        raw_image.append(ds)
+    if B is not None:
+        src_ds_B = gdal.Open(B, gdal.GA_ReadOnly)
+        for band in range(src_ds_B.RasterCount):
+            ds = src_ds_B.GetRasterBand(band + 1).ReadAsArray().flatten().astype(np.float32)
+            if nodata is not None:
+                ds[ds == nodata] = band_mean[band]
+            raw_image.append(ds)
+
     @dask.delayed
     def get_principal_component(i, j):
-        return eigenvectors[j, i] * (raw_image[j] - center_value)
+        return eigenvectors[j, i] * (raw_image[j] - band_mean[j])
 
     pca_files = []
     for i in range(n_pc):
@@ -124,6 +156,9 @@ def pca(A, B, n_pc, estimator_matrix, out_dir, n_threads, block_size, nodata=Non
         del pcband, out_pc
 
         pca_files.append(tmp_pca_file)
+
+    # free mem
+    del raw_image, src_ds_A, src_ds_B, ds
 
     # compute the pyramids for each pc image
     @dask.delayed
