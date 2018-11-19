fa66bb7f1daf1737186394acab7a706485fafcef
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2018-11-19 18:30:29 -0500

    Increase the precision in range values to nine decimals

--- gui/component_analysis_dialog.py
+++ gui/component_analysis_dialog.py
@@ -441,8 +441,8 @@ class ComponentAnalysisDialog(QWidget, FORM_CLASS):
         # update statistics and histogram plot
         self.set_statistics(stats_for="Areas Of Interest")
         # update range values using min/max of AOI with decimal adjusted for include in change layer
-        range_from = np.floor(np.min(self.aoi_data)*1000)/1000
-        range_to = np.ceil(np.max(self.aoi_data)*1000)/1000
+        range_from = np.floor(np.min(self.aoi_data)*1000000000)/1000000000
+        range_to = np.ceil(np.max(self.aoi_data)*1000000000)/1000000000
         # double set due the synchronization values from plot min/max adjust
         self.RangeChangeFrom.setValue(range_from)
         self.RangeChangeTo.setValue(range_to)
